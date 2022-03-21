99d35bd2c6c2fd3361936149603869569468e3a7
yashiverma509 <101495215+yashiverma509@users.noreply.github.com>
2022-03-21 10:50:12 +0300

    Create A4.java

---
+++ question4/A4.java
@@ -0,0 +1,10 @@
+package opps;
+
+public class A4 {
+	public static void main(String[] args) {
+		Employee employee=new
+				CommisionEmployee(121, "amit", 2.1, 2000000);
+		System.out.println(employee.getPayment());
+
+} 
+	}

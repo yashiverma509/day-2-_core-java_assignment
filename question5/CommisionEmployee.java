96f1e73fdd460b6c1b92c2bd161c1efd4e818e92
yashiverma509 <101495215+yashiverma509@users.noreply.github.com>
2022-03-21 10:58:23 +0300

    Create CommisionEmployee.java

---
+++ question5/CommisionEmployee.java
@@ -0,0 +1,18 @@
+package opps;
+
+public class CommisionEmployee extends Employee {
+	private double commisionPercentage;
+	private double totalSales;
+
+	public CommisionEmployee(int id, String name, double commisionPercentage, double totalSales) {
+		super(id, name);
+		this.commisionPercentage = commisionPercentage;
+		this.totalSales = totalSales;
+	}
+
+	@Override
+	public double getPayment() {
+		return totalSales * commisionPercentage / 100;
+	}
+
+}

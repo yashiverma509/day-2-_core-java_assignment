fb9a413361aca81379b02066c1394fe3f9e0b2f9
yashiverma509 <101495215+yashiverma509@users.noreply.github.com>
2022-03-21 11:02:40 +0300

    Create PaymentProcessingSystem.java

---
+++ question5/PaymentProcessingSystem.java
@@ -0,0 +1,9 @@
+package opps;
+
+public class PaymentProcessingSystem {
+	public static void processPayment(Payble payable) {
+		
+		System.out.println("payment of total: "+ payable.getPayment()+" is processed");
+	}
+
+}

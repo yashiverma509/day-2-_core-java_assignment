3adae034ac7aa3ee0cd142ca22fb349e8729b309
yashiverma509 <101495215+yashiverma509@users.noreply.github.com>
2022-03-21 10:25:19 +0300

    Create A3.java

---
+++ question3/A3.java
@@ -0,0 +1,16 @@
+package a;
+
+public class A3 {
+	
+
+		public static void main(String[] args) {
+			
+			Account account=new
+					SavingsAccount("Janvi", "4343433434", 30000, 5, 1000);
+		
+			
+			Account account2=new 
+				CurrentAccount("Shreya", "543545445", 500000, "AB1234", 50000);
+			
+		}
+	}
